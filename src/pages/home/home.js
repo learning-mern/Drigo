4c701070ad7e031de62241f5aa006826d4a0a272
Manish Pandey <2018140@iiitdmj.ac.in>
2020-12-26 13:33:48 +0530

    Designed Landing Page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import "./home.css";
 
 
 export default class Home extends React.Component {
     render(){
         return(
-            <div>
-                <p> Drivo </p>
+            <div style = {{width: "100vw", height: "100vh"}} className = "background">
+                <p className = "logo"> Drivo </p>
+                <p className = "welcome"> Welcome to Drivo! </p>
+                <p className = "login"> Login </p>
+                <p className = "signup"> Signup </p>
             </div>
         )
     }
