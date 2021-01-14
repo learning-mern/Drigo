3aa5a349d535bb2c37702b0431c320e8a01b2c5d
Manish Pandey <2018140@iiitdmj.ac.in>
2021-01-14 15:07:08 +0530

    Added Firebase Signup for Passenegrs

--- src/pages/Passenger/signup/signup.js
+++ src/pages/Passenger/signup/signup.js
@@ -1,24 +1,62 @@
 import React from 'react'
 import "./signup.css";
+import firebase from "../../../services/firebase"
 
 
 export default class BookAJourneySignup extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            name: "",
+            error: null
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    async handleSubmit(event){
+        const {name, password, email} = this.state;
+        event.preventDefault();
+        try{
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(async result => {
+                    localStorage.setItem('name', name);
+                    localStorage.setItem('email', email);
+
+                    console.log(localStorage.getItem('name'));
+                    console.log(localStorage.getItem('email'));
+
+                    this.props.history.push("./login");
+            })
+        } catch(error){
+            console.error("Error is: ", error);
+        }
+    }
 
     render(){
         return(
             <div style = {{width: "100vw", height: "100vh"}} className = "background">
                 <p className = "logo"> Drigo </p>
                 <div style = {{width: "280px", height: "460px"}} className = "loginDiv">
-                    <form className="loginForm" noValidate>
+                    <form className="loginForm" noValidate onSubmit={this.handleSubmit}>
                         <h1 class="">hey buddy !!</h1>
                         <input
                             type="name"
-                            id="email"
-                            name="email"
+                            id="name"
+                            name="name"
                             class="form-control"
                             placeholder="NAME"
                             required
-                            value = ""
+                            onChange = {this.handleChange}
+                            value = {this.state.name}
                             autofocus />
                         <input
                             type="email"
@@ -27,19 +65,21 @@ export default class BookAJourneySignup extends React.Component {
                             class="form-control"
                             placeholder="EMAIL ADDRESS"
                             required
-                            value = ""
-                            autofocus />
+                            onChange = {this.handleChange}
+                            value = {this.state.email} />
                         <input
                             type="password"
                             id="password"
                             name="password"
                             class="form-control"
                             placeholder="PASSWORD"
-                            value = ""
+                            value = {this.state.password}
+                            onChange = {this.handleChange}
                             required />
                         <button
                             class="login-button"
-                            type="submit">
+                            type="submit"
+                            >
                             Signup
                         </button>
                         <p class="mt-5 mb-3 dont-have-acc">Already have an account?</p>
