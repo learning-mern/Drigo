7f6cc01b316eec46d6c0169b30e012aa0a467e17
Manish Pandey <2018140@iiitdmj.ac.in>
2021-01-15 14:47:24 +0530

    Added Firebase Signup for Driver

--- src/pages/Driver/signup/signup.js
+++ src/pages/Driver/signup/signup.js
@@ -1,24 +1,75 @@
 import React from 'react'
 import "./signup.css";
+import firebase from "../../../services/firebase"
 
 
 export default class IamADrigoSignup extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            name: "",
+            vehicleNum: "",
+            error: null
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    async handleSubmit(event){
+        const {name, password, email, vehicleNum} = this.state;
+        event.preventDefault();
+        try{
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(async result => {
+                firebase.firestore().collection('driver').doc('users').collection('users')
+                .add({
+                    name,
+                    email,
+                    vehicleNum
+                }).then(async result => {
+                        localStorage.setItem('name', name);
+                        localStorage.setItem('email', email);
+                        localStorage.setItem('password', password);
+                        localStorage.setItem('vehicleNum', vehicleNum);
+
+                        console.log(localStorage.getItem('name'));
+                        console.log(localStorage.getItem('email'));
+
+                        this.props.history.push("./");
+                }).catch((error) => {
+                    console.error("Error adding document: ", error);
+                })
+            })
+        } catch(error){
+            console.error("Error in adding user: ", error);
+        }
+    }
 
     render(){
         return(
             <div style = {{width: "100vw", height: "100vh"}} className = "background">
                 <p className = "logo"> Drigo </p>
                 <div style = {{width: "280px", height: "510px"}} className = "loginDiv">
-                    <form className="loginForm" noValidate>
+                    <form className="loginForm" noValidate onSubmit={this.handleSubmit}>
                         <h1 class="">hey buddy !!</h1>
                         <input
                             type="name"
-                            id="email"
-                            name="email"
+                            id="name"
+                            name="name"
                             class="form-control"
                             placeholder="NAME"
                             required
-                            value = ""
+                            onChange = {this.handleChange}
+                            value = {this.state.name}
                             autofocus />
                         <input
                             type="email"
@@ -27,7 +78,8 @@ export default class IamADrigoSignup extends React.Component {
                             class="form-control"
                             placeholder="EMAIL ADDRESS"
                             required
-                            value = ""
+                            onChange = {this.handleChange}
+                            value = {this.state.email}
                             autofocus />
                         <input
                             type="password"
@@ -35,7 +87,8 @@ export default class IamADrigoSignup extends React.Component {
                             name="password"
                             class="form-control"
                             placeholder="PASSWORD"
-                            value = ""
+                            value = {this.state.password}
+                            onChange = {this.handleChange}
                             required />
                         <input
                             type="vehicleNum"
@@ -43,7 +96,8 @@ export default class IamADrigoSignup extends React.Component {
                             name="vehicleNum"
                             class="form-control"
                             placeholder="VEHICLE NUMBER"
-                            value = ""
+                            value = {this.state.vehicleNum}
+                            onChange = {this.handleChange}
                             required />
                         <button
                             class="login-button"
